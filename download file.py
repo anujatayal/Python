56570f5986d535cab80dcc05c99744bab84115ea
anujatayal <anujatayal@gmail.com>
2019-02-07 14:14:41 +0530

    Update download file.py

--- download file.py
+++ download file.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+#specify url to download
 url='http://codex.cs.yale.edu/avi/db-book/db4/slide-dir/ch1-2.pdf'
 resp=requests.get(url,stream=True)
+#specify file name of download 
 with open("python_logo.pdf",'wb') as f:
     for chunk in resp.iter_content(chunk_size=1024):
         if chunk:
